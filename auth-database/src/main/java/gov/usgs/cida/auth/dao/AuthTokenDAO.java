a373e6979791d197507c6e015bc64f854f147395
isuftin <isuftin@usgs.gov>
2014-08-19 13:58:20 -0500

    Basic CRUD operations in place

--- auth-database/src/main/java/gov/usgs/cida/auth/dao/AuthTokenDAO.java
+++ auth-database/src/main/java/gov/usgs/cida/auth/dao/AuthTokenDAO.java
@@ -2,6 +2,7 @@ package gov.usgs.cida.auth.dao;
 
 import gov.usgs.cida.auth.model.AuthToken;
 import gov.usgs.cida.auth.util.MyBatisConnectionFactory;
+import java.math.BigInteger;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -23,6 +24,10 @@ public class AuthTokenDAO {
         sqlSessionFactory = factory;
     }
 	
+	/**
+	 * Gets all available authentication tokens
+	 * @return 
+	 */
 	public List<AuthToken> getAll() {
 		List<AuthToken> result;
 		
@@ -30,6 +35,32 @@ public class AuthTokenDAO {
 			result = session.selectList("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.getAll");
 		}
 		
+		return result;
+	}
+	
+	public AuthToken getByTokenId(String id) {
+		AuthToken result;
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			result = session.selectOne("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.getByTokenId", id);
+		}
+		return result;
+	}
+	
+	public int deleteTokenUsingId(String id) {
+		int result;
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			result = session.delete("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.deleteTokenUsingId", id);
+			session.commit();
+		}
+		return result;
+	}
+	
+	public int insertToken(AuthToken token) {
+		int result;
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			result = session.insert("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.insertToken", token);
+			session.commit();
+		}
 		return result;
 	}
 	
--- auth-database/src/test/java/gov/usgs/cida/auth/dao/AuthTokenDAOIT.java
+++ auth-database/src/test/java/gov/usgs/cida/auth/dao/AuthTokenDAOIT.java
@@ -3,14 +3,17 @@ package gov.usgs.cida.auth.dao;
 import gov.usgs.cida.auth.model.AuthToken;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -24,7 +27,8 @@ import org.junit.experimental.categories.Category;
 public class AuthTokenDAOIT {
 
 	private static SqlSessionFactory sqlSessionFactory;
-
+	private AuthTokenDAO dao;
+	
 	public AuthTokenDAOIT() {
 	}
 
@@ -43,6 +47,7 @@ public class AuthTokenDAOIT {
 
 	@Before
 	public void setUp() {
+		dao = new AuthTokenDAO(sqlSessionFactory);
 	}
 
 	@After
@@ -52,10 +57,59 @@ public class AuthTokenDAOIT {
 	@Test
 	public void testGetAll() {
 		System.out.println("getAll");
-		AuthTokenDAO dao = new AuthTokenDAO(sqlSessionFactory);
 		List<AuthToken> result = dao.getAll();
 		assertThat(result, is(notNullValue()));
-		assertThat(result.size(), is(equalTo(100)));
+		assertThat(result.size(), greaterThan(1));
 	}
+
+	@Test
+	public void testGetByTokenId() {
+		System.out.println("getByTokenId");
+		AuthToken result = dao.getByTokenId("88AD43FE-58FA-12E8-41C1-72F0E20D9F1F");
+		assertThat(result, is(notNullValue()));
+		assertThat(result.getUsername(), is(equalTo("lobortis@diam.com")));
+	}
+	
+	@Test
+	public void testDeleteTokenUsingId() {
+		System.out.println("deleteTokenUsingId");
+		int result = dao.deleteTokenUsingId("BCFCAA99-18D7-5833-FD50-AFE27E7AF1ED");
+		assertThat(result, is(equalTo(1)));
+	}
+	
+	@Test
+	public void testInsertToken() {
+		System.out.println("insertToken");
+		Calendar cal = Calendar.getInstance();
+		Date dt = new Date();
+		long now = dt.getTime();
+		
+		cal.setTime(dt);
+		cal.add(Calendar.DATE, 1);
+		long tomorrow = cal.getTimeInMillis();
+		
+		AuthToken token = new AuthToken();
+		String tokenId = "TEST-TOKEN-ID";
+		String username = "isuftin@usgs.gov";
+		
+		token.setTokenId(tokenId);
+		token.setUsername(username);
+		token.setIssued(new Timestamp(now));
+		token.setExpires(new Timestamp(tomorrow));
+		token.setLastAccess(new Timestamp(now));
+		
+		int insertCount = dao.insertToken(token);
+		assertThat(insertCount, is(equalTo(1)));
+		
+		AuthToken result = dao.getByTokenId(tokenId);
+		assertThat(result, is(notNullValue()));
+		assertThat(result.getTokenId(), is(equalTo(tokenId)));
+		assertThat(result.getUsername(), is(equalTo(username)));
+		assertThat(result.getIssued().getTime(), is(equalTo(now)));
+		assertThat(result.getExpires().getTime(), is(equalTo(tomorrow)));
+		assertThat(result.getLastAccess().getTime(), is(equalTo(now)));
+		
+	}
+	
 
 }
