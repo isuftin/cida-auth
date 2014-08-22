151f05da11cbd8a7a51c57418c1af25f59a2f392
Suftin, Ivan <isuftin@usgs.gov>
2014-08-21 20:20:04 -0500

    Added services to update token expiration date

--- cida-auth-webservice/src/main/java/gov/usgs/cida/auth/webservice/token/TokenService.java
+++ cida-auth-webservice/src/main/java/gov/usgs/cida/auth/webservice/token/TokenService.java
@@ -2,8 +2,11 @@ package gov.usgs.cida.auth.webservice.token;
 
 import gov.usgs.cida.auth.dao.AuthTokenDAO;
 import gov.usgs.cida.auth.model.AuthToken;
+import gov.usgs.cida.auth.util.AuthTokenFactory;
+import java.util.Calendar;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,6 +22,7 @@ import org.slf4j.LoggerFactory;
 @Path("/")
 public class TokenService {
 	private final static Logger LOG = LoggerFactory.getLogger(TokenService.class);
+	private final static String ONE_DAY_IN_SECONDS_PARAM = "86400";
 	/**
 	 *
 	 * @param tokenId
@@ -35,6 +39,32 @@ public class TokenService {
 		
 		if (token != null) {
 			response = Response.ok(token.toJSON(), MediaType.APPLICATION_JSON_TYPE).build();
+		} else {
+			response = Response.status(Response.Status.NOT_FOUND).build();
+		}
+		
+		return response;
+	}
+	
+	@PUT
+	@Path("/extend/{tokenId}/{seconds}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response extendToken(
+			@PathParam("tokenId") @DefaultValue("no token provided") String tokenId,
+			@PathParam("seconds") int seconds) {
+		LOG.trace("Attempting to extent token {}", tokenId);
+		Response response;
+		AuthTokenDAO dao = new AuthTokenDAO();
+		AuthToken token = dao.getByTokenId(tokenId);
+		
+		if (token != null) {
+			token.extendExpiration(seconds);
+			int updated = dao.updateTokenExpiration(token);
+			if (updated == 0) {
+				response = Response.serverError().build();
+			} else {
+				response = Response.ok(token.toJSON(), MediaType.APPLICATION_JSON_TYPE).build();
+			}
 		} else {
 			response = Response.status(Response.Status.NOT_FOUND).build();
 		}
