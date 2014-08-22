7e2871299976d0eaebcb328a6488b70dc2e16ed1
Suftin, Ivan <isuftin@usgs.gov>
2014-08-21 21:05:37 -0500

    Added documentation for AuthTokenDAO

--- auth-database/src/main/java/gov/usgs/cida/auth/dao/AuthTokenDAO.java
+++ auth-database/src/main/java/gov/usgs/cida/auth/dao/AuthTokenDAO.java
@@ -12,31 +12,39 @@ import org.apache.ibatis.session.SqlSessionFactory;
  * @author isuftin
  */
 public class AuthTokenDAO {
-	private final SqlSessionFactory sqlSessionFactory;
-	
-	public AuthTokenDAO() {
-        sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
-    }
 
-    public AuthTokenDAO(SqlSessionFactory factory) {
-		
-        sqlSessionFactory = factory;
-    }
-	
+	private final SqlSessionFactory sqlSessionFactory;
+
+	public AuthTokenDAO() {
+		sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+	}
+
+	public AuthTokenDAO(SqlSessionFactory factory) {
+
+		sqlSessionFactory = factory;
+	}
+
 	/**
 	 * Gets all available authentication tokens
-	 * @return 
+	 *
+	 * @return
 	 */
 	public List<AuthToken> getAll() {
 		List<AuthToken> result;
-		
+
 		try (SqlSession session = sqlSessionFactory.openSession()) {
 			result = session.selectList("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.getAll");
 		}
-		
+
 		return result;
 	}
-	
+
+	/**
+	 * Gets an authentication token based on an ID
+	 *
+	 * @param id
+	 * @return
+	 */
 	public AuthToken getByTokenId(String id) {
 		AuthToken result;
 		try (SqlSession session = sqlSessionFactory.openSession()) {
@@ -44,7 +52,13 @@ public class AuthTokenDAO {
 		}
 		return result;
 	}
-	
+
+	/**
+	 * Deletes a token based on a token ID
+	 *
+	 * @param id
+	 * @return 1 if deleted, 0 if not
+	 */
 	public int deleteTokenUsingId(String id) {
 		int result;
 		try (SqlSession session = sqlSessionFactory.openSession()) {
@@ -53,32 +67,51 @@ public class AuthTokenDAO {
 		}
 		return result;
 	}
-	
+
+	/**
+	 * Inserts an AuthToken
+	 *
+	 * @param token
+	 * @return 1 if inserted, 0 if not
+	 */
 	public int insertToken(AuthToken token) {
 		int result;
 		try (SqlSession session = sqlSessionFactory.openSession()) {
 			result = session.insert("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.insertToken", token);
 			session.commit();
-		} 
+		}
 		return result;
 	}
-	
+
+	/**
+	 * Updates AuthToken expiration based on the expiration field in the token
+	 *
+	 * @param token
+	 * @return 1 if updated, 0 if not
+	 */
 	public int updateTokenExpiration(AuthToken token) {
 		int result;
 		try (SqlSession session = sqlSessionFactory.openSession()) {
 			result = session.update("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.updateTokenExpiration", token);
 			session.commit();
-		} 
+		}
 		return result;
 	}
 
+	/**
+	 * Updates the AuthToken last access based on the last access field in the
+	 * token
+	 *
+	 * @param token
+	 * @return
+	 */
 	public int updateTokenLastAccess(AuthToken token) {
 		int result;
 		try (SqlSession session = sqlSessionFactory.openSession()) {
 			result = session.update("gov.usgs.cida.mybatis.mappers.AuthTokenMapper.updateTokenLastAccess", token);
 			session.commit();
-		} 
+		}
 		return result;
 	}
-	
+
 }
