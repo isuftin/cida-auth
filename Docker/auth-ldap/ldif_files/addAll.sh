30ab3afd8d8bc1a7923bf977383c725b744e31d8
Ivan Suftin <isuftin@usgs.gov>
2016-07-12 10:57:08 -0500

    Finished creating the cida_auth_group and added two more users to it

--- Docker/auth-ldap/ldif_files/addAll.sh
+++ Docker/auth-ldap/ldif_files/addAll.sh
@@ -7,3 +7,5 @@ sleep 5
 ldapadd -h ldap-server -c -D cn=admin,dc=gs,dc=doi,dc=net -w admin -f /ldif_files/groups.ldif  || true
 
 ldapadd -h ldap-server -c -D cn=admin,dc=gs,dc=doi,dc=net -w admin -f /ldif_files/people.ldif  || true
+
+ldapmodify -h ldap-server -c -D cn=admin,dc=gs,dc=doi,dc=net -w admin -f /ldif_files/addUsersToGroup.ldif  || true
